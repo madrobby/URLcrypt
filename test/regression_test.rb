be32f132fcfb9122ba1051d599db193b596e39ca
Thomas Cannon <tcannon00@gmail.com>
2019-04-29 09:47:50 -0400

    Extra regression test to confirm encrypt/decrypt with a too long key

--- test/regression_test.rb
+++ test/regression_test.rb
@@ -19,4 +19,18 @@ class URLcryptRegressionTest < TestClass
     encrypted = URLcrypt::encrypt(original)
     assert_equal(URLcrypt::decrypt(encrypted), original)
   end
+
+  def test_encryption_and_decryption_with_too_long_key
+    # this key was generated via rake secret in a rails app, the pack() converts it into a byte array
+    secret = ['d25883a27b9a639da85ea7e159b661218799c9efa63069fac13a6778c954fb6d721968887a19bdb01af8f59eb5a90d256bd9903355c20b0b4b39bf4048b9b17b'].pack('H*')
+    URLcrypt::key = secret
+
+    assert OpenSSL::Cipher.new('aes-256-cbc').key_len < secret.bytesize
+
+    original = '{"some":"json_data","token":"dfsfsdfsdf"}'
+    encrypted = URLcrypt.encrypt(original)
+
+    encrypted = URLcrypt::encrypt(original)
+    assert_equal(URLcrypt::decrypt(encrypted), original)
+  end
 end
